e7f3941594c90d55895029133fb8f84e9a607330
noomesk <aplazasn@unal.edu.co>
2026-01-30 23:18:57 -0500

    Update organizar_fotos.py

--- organizar_fotos.py
+++ organizar_fotos.py
@@ -1,10 +1,10 @@
 import os
 import shutil
 
-# Usa esta ruta (cámbiala por la TUYA dependiendo de lo que vayas a organizar amigue):
+# Usar ruta y revisar procesos LABFISIO (cámbiarla dependiendo de lo que vayas a organizar amigue):
 ruta_fotos = r"C:\Users\A\serpientes"  # ¡Observa la 'r' al inicio y sin doble \\ eso ayuda a que no se confunda con el \ que python lo interpreta como un escape)
 
-# Crea la carpeta "Ordenadas" si no existe
+# Crearr una carptea"Ordenadas" si no existe
 os.makedirs(os.path.join(ruta_fotos, "Ordenadas"), exist_ok=True)
 
 for archivo in os.listdir(ruta_fotos):
@@ -14,7 +14,7 @@ for archivo in os.listdir(ruta_fotos):
                 os.path.join(ruta_fotos, archivo),
                 os.path.join(ruta_fotos, "Ordenadas", archivo)
             )
-            print(f"✅ Movido: {archivo}")
+            print(f"✅ Moviido: {archivo}")
         except Exception as e:
             print(f"❌ Error con {archivo}: {str(e)}")
 
